3f4b737465eca9caa744d11d668a8c2b51180d5e
farhanshahriyar <farhan.abir9999@gmail.com>
2023-09-11 13:35:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'; // Importing CSS Styles
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold underline text-blue-500">
+      Doctor Appointment App
+      
+    </h1>
+    </>
+  )
+}
+
+export default App
