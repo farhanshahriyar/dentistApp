eaf60b69b33015b2658f75d2ff91ae2593c4f756
farhanshahriyar <farhan.abir9999@gmail.com>
2023-09-11 13:42:51 +0600

    project setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css'; // Importing CSS Styles
+import Layout from './layout/Layout';
 
 function App() {
 
 
   return (
     <>
-      <h1 className="text-2xl font-bold underline text-blue-500">
-      Doctor Appointment App
-      
-    </h1>
+      <Layout/>
     </>
   )
 }
