e2e730584c12b02fd9bec0d2d99d4309bc95c275
farhanshahriyar <farhan.abir9999@gmail.com>
2023-09-11 20:55:27 +0600

    navigation work done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -24,8 +24,30 @@ const navLinks = [
 ];
 
 const Header = () => {
+
+  const headerRef = useRef(null);
+  const menuRef = useRef(null);
+
+  const handleStickyHeader = () => {
+    window.addEventListener("scroll", () => {
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        headerRef.current.classList.add("sticky__header");
+      } else {
+        headerRef.current.classList.remove("sticky__header");
+      }
+    });
+  };
+
+  useEffect(() => {
+    handleStickyHeader();
+
+  return () => window.removeEventListener("scroll", handleStickyHeader);
+  });
+
+  const toggleMenu = () => menuRef.current.classList.toggle("show__menu");
+
   return (
-    <header className="header flex items-center">
+    <header className="header flex items-center" ref={headerRef}>
       <div className="container">
         <div className="flex items-center justify-between">
           {/* ===== logo ===== */}
@@ -34,7 +56,7 @@ const Header = () => {
           </div>
 
           {/* ===== menu ===== */}
-          <div className="navigation">
+          <div className="navigation" ref={menuRef} onClick={toggleMenu}>
             <ul className="menu flex items-center gap-[2.7rem]">
               {navLinks.map((link, index) => (
                 <li key={index}>
@@ -55,7 +77,7 @@ const Header = () => {
 
           {/* ===== nav right ===== */}
           <div className="flex items-center gap-4">
-             <div>
+             <div className="hidden">
               <Link to="/">
                 <figure className="w-[35px] h-[35px] rounded-full cursor-pointer">
                   <img src={userImg} className="w-full rounded-full" alt="user"  />
@@ -68,7 +90,7 @@ const Header = () => {
                 Login</button>
              </Link>
 
-             <span className="md:hidden">
+             <span className="md:hidden" onClick={toggleMenu}>
               <BiMenu className="w-6 h-6 cursor-pointer"/>
               </span>
           </div>
