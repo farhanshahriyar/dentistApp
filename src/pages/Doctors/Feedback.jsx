695932972a15a3d94e53e77f7f94739deb4368a0
farhanshahriyar <farhan.abir9999@gmail.com>
2023-09-17 20:40:50 +0600

    added feedback functionalities

--- src/pages/Doctors/Feedback.jsx
+++ src/pages/Doctors/Feedback.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { useState } from 'react'
+import avatar from '../../assets/images/avatar-icon.png'
+import { formateDate } from '../../utils/formateDate'
+import { AiFillStar } from 'react-icons/ai'
+import FeedbackForm from './FeedbackForm'
 
 const Feedback = () => {
+  const [showFeedbackForm, setShowFeedbackForm] = useState(false)
   return (
     <div>
-      Feedback
+        <div className="mb-[50px]">
+            <h3 className='text-[20px] leading-[30px] text-headingColor font-bold'>All Reviews (272)</h3>
+        </div>
+
+        <div className='flex justify-between gap-10 mb-[30px]'>
+            <div className='flex gap-3'>
+              <figure className='w-10 h-10 rounded-full'>
+                <img className='w-full' src={avatar} alt='avatar' />
+              </figure>
+              <div>
+                <h4 className='text-[16px] leading-6 text-primaryColor font-bold'>Dr. Saleh Mahmud</h4>
+                <p className='text-[14px] leading-5 text-textColor font-medium'>{formateDate("02-14-2023")}</p>
+                <p className='text__para mt-3 font-medium text-[15px] text-textColor'>Good services, highly recommended</p>
+              </div>
+            </div>
+            <div className='flex gap-1'>
+              {
+                [...Array(5).keys()].map((_, index) => 
+                    <AiFillStar key={index} color='#0067FF' />)
+
+                }
+            </div> 
+        </div>
+
+        {/* <div className='text-center'>
+            <button className='bg-irisBlueColor text-white rounded-full px-10 py-3 font-bold' onClick={()=>setShowFeedbackForm(true)}>Give Feedback</button>
+        </div> */}
+
+        {!showFeedbackForm && (
+          <div className='text-center'>
+            <button className='bg-irisBlueColor text-white rounded-full px-10 py-3 font-bold' onClick={()=>setShowFeedbackForm(true)}>Give Feedback</button>
+          </div>
+        )}
+
+        {showFeedbackForm && <FeedbackForm/>}
     </div>
   )
 }
---
+++ src/pages/Doctors/FeedbackForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FeedbackForm = () => {
+  return (
+    <div>
+      FeedbackForm
+    </div>
+  )
+}
+
+export default FeedbackForm
