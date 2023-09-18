8f37e5f2139df2b24436dd894fb331a0f0bd03e6
farhanshahriyar <farhan.abir9999@gmail.com>
2023-09-18 13:58:45 +0600

    frontend 90% done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-        Contact
-    </div>
+   <section className="">
+      <div className="px-4 mx-auto max-w-screen-md">
+          <h2 className="heading text-center">Contact Us</h2>
+          <p className="mb-8 lg:mb-16 font-light text-center text__para">
+            Got a technical issue or inquiry? We are here to help. Fill out the form below and we will get back to you as soon as possible.
+          </p>
+          <form className="space-y-8">
+              <div>
+                  <label className="form__label">Your Email</label>
+                  <input type="email" className="form__input" placeholder="example@gmail.com" />
+              </div>
+              <div>
+                  <label className="form__label">Subject</label>
+                  <input type="text" className="form__input mt-1" placeholder="Let us know how we can help you?" />
+              </div>
+
+              <div className="sm:col-span-2">
+                  <label className="form__label">Your Message</label>
+                  <textarea type="text" 
+                  rows="6" 
+                  className="form__input mt-1" 
+                  placeholder="Leave a comment...."></textarea>
+              </div>
+              <button type="submit" className="btn rounded sm:w-fit">Submit</button>
+          </form>
+      </div>
+   </section>
   )
 }
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -28,7 +28,9 @@ const Signup = () => {
 
     const handleFileInputChange = async (event) => {
       const file = event.target.files[0];
-      console.log(file)
+
+      // later we will add cloudinary to upload images
+      // console.log(file)  // works file
     }
 
     const submitHandler = async event => {
@@ -48,7 +50,7 @@ const Signup = () => {
           {/* === form box === */}
           <div className='rounded-r-lg lg:pl-16 py-10'>
             <h3 className='text-headingColor text-[22px] leading-9 font-bold mb-10'>Create an <span className='text-primaryColor'>account</span></h3>
-            
+
             <form onSubmit={submitHandler}>
               <div className="mb-5">
                 <input type="text" name='text' value={FormData.name} onChange={handleInpurChange}
